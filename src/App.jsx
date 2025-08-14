b8bc1af0f17a146dd6f3fcc1273cc963cf0f708c
Ponkel33 <the.orangutans.w@gmail.com>
2025-08-14 16:53:19 +0900

    詳細ページの作成、トップページから詳細ページへのリンク作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './Pages/Header'
 import { Home } from './Pages/Home'
 import { Contact } from './Pages/Contact'
+import { Post } from './Pages/Post'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
